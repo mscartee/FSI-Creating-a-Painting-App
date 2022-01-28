eac56800aa88a90cfc90ce604583b9737e3d992b
Michael Cartee <admin@asgardcrolingai.lan>
2022-01-27 21:43:06 -0500

    Done

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ painting.addEventListener('click',function(e){
     //console.log(e.target)
     e.target.style.backgroundColor=selectedColor
 })
+painting.addEventListener('dblclick', function(e){
+    e.target.style.backgroundColor='white'
+})
 
 let palleteColor=document.querySelector('.palette')
 palleteColor.addEventListener('click',function(e){
